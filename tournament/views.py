1bac566d5b30b235acc7ad53d5318b91c5487f0a
Jimbomaniak <jimboman54321@gmail.com>
2016-12-16 16:36:47 +0200

    add  requirePOST, add stage logic in create_tournament

--- tournament/views.py
+++ tournament/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from .models import Tournament, Stage, Player
-from .forms import NewTournament
+from .forms import NewTournament, NewStage
+from django.views.decorators.http import require_POST
 
 
 def main(request):
@@ -12,18 +13,25 @@ def main(request):
     is_tourn_admin = request.user.groups.filter(name='tournament_admin')
     if is_tourn_admin:
         tournament_form = NewTournament()
+        stage_form = NewStage()
         context['new_tourn_form'] = tournament_form
         context['is_tourn_admin'] = is_tourn_admin
-
+        context['new_stage_form'] = stage_form
     return render(request, 'tournament/main.html', context)
 
 
+@require_POST
 def create_tournament(request):
-    if request.method == 'POST':
-        tournament_form = NewTournament(request.POST)
-        if tournament_form.is_valid():
-            tournament_form.save()
-            return redirect('main')
+    tournament_form = NewTournament(request.POST)
+    if tournament_form.is_valid():
+        tournament_form = tournament_form.save()
+        stage_form = NewStage(request.POST)
+        new_tournament_id = stage_form.save(commit=False)
+        new_tournament_id.tournament_id = tournament_form.id
+        new_tournament_id.save()
+        stage_form.save_m2m()
+        return redirect('main')
+    return redirect('main')
 
 
 def create_teams(request):
@@ -62,3 +70,6 @@ def players(request):
     get_players = Player.objects.all()
     context = {'players': get_players}
     return render(request, 'tournament/players.html', context)
+
+
+
