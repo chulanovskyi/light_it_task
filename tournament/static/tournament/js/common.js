7a970b89cebe270d87d1ff60ff07afbb36db8837
Jimbomaniak <jimboman54321@gmail.com>
2017-01-13 13:35:21 +0200

    remove tournament jquery

--- tournament/static/tournament/js/common.js
+++ tournament/static/tournament/js/common.js
@@ -7,25 +7,4 @@ $(document).ready(function(){
 $('#dataTable tr td:not(:last-child)').click(function ()    {
  location.href = $(this).parent().find('td a').attr('href');
 });
-*/
 
-/* For Create-tournament FORM */
-$(document).ready(function(){
-  $('.modal-body form').submit(function(){
-    var x = 0;
-    $('li label input').each(function(){
-      if ($(this).prop('checked')) {
-        x += 1
-     }
-    });
-    if (x<4) {
-      alert('You must choose 4 players at least');
-      return false
-    }
-    else if (x%2 !== 0) {
-      alert('Сhoose only an even number of players');
-      return false
-    }
-    return true
-  })
-});
