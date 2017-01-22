4e9e2be6d1a9c4650ce5616416a768271d3f2917
Jimbomaniak <jimboman54321@gmail.com>
2017-01-22 22:43:50 +0200

    add new urls according to views

--- tournament/urls.py
+++ tournament/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 
-urlpatterns = [
+'''urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^profile/', views.edit_profile, name='profile'),
     url(r'^players/$', views.players, name='players'),
@@ -15,4 +15,12 @@ urlpatterns = [
     url(r'^(?P<tournament_name>[\w\s]+)/matches/(?P<stage_id>[0-9]+)/$', views.create_matches, name='matches'),
     url(r'^(?P<tournament_name>[\w\s]+)/(?P<stage_id>[0-9]+)/match-list/$', views.show_matches, name='show'),
     url(r'^(?P<tournament_name>[\w\s]+)/(?P<stage_id>[0-9]+)/match-list/match_score/$', views.match_score, name='score'),
+]'''
+
+
+urlpatterns = [
+    url(r'^$', views.TournamentList.as_view(), name='main'),
+    url(r'^tourn(?P<tourn_id>[0-9]+)/$', views.StageList.as_view(), name='tournament'),
+    url(r'^tourn(?P<tourn_id>[0-9]+)/stage(?P<stage_id>)[0-9]+/$', views.TableList.as_view(), name='table'),
+    url(r'^tourn(?P<tourn_id>[0-9]+)/stage(?P<stage_id>)[0-9]+/matches/$', views.MatchesList.as_view(), name='matches'),
 ]
