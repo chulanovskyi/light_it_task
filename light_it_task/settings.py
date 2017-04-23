47fe25d9298e410813aa023817ff75e39fcb6c8f
chulanovskyi <skyrocker@mail.ru>
2017-04-23 22:56:27 +0300

    psql set

--- light_it_task/settings.py
+++ light_it_task/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'light_it_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dfhc251rh19p9g',
+        'HOST': 'ec2-46-137-97-169.eu-west-1.compute.amazonaws.com',
+        'USER': 'cxymbojwyajwll',
+        'PASSWORD': '90d249cb7cb1bbde4d3a81ed0ef63b9894db9dbbadb04c1a9449673acef08d49',
+        'PORT': '5432',
     }
 }
 
