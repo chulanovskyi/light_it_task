9a97b07899649b0a5d5b8bcb40345c0276e30e04
chulanovskyi <skyrocker@mail.ru>
2017-04-24 13:28:15 +0300

    fix set path

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "light_it_task.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "light_it_task.sets.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
