d40788ced2ca2b728fe73a1824e9d23235637f9c
chulanovskyi <skyrocker@mail.ru>
2017-04-24 11:28:28 +0300

    static fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --pythonpath light_it_task light_it_task.wsgi
+web: gunicorn --pythonpath light_it_task light_it_task.wsgi; python manage.py collectstatic --noinput
--- light_it_task/urls.py
+++ light_it_task/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     url(r'^core/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^', include('tournament.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
